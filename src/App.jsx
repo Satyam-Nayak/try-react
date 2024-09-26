cc29adf2943f85d2c3d6edc4672153e81243ac8a
Satyam-Nayak <satyamn120@gmail.com>
2024-09-26 09:44:48 +0530

    26-09-2024 applying css connect

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import './App.css';
 import Home from './Component1';
 import Login from './Login';
 import Ccompo2 from './Compo2';
+import Car from './Car';
 function App() {
   return (
     <div>
+      <Car></Car>
     <Home></Home>
     <Ccompo2></Ccompo2> 
     {/* using class functions */}
---
+++ src/Car.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+import './Car.css';
+
+let Car = () =>{
+    return(
+        <div className="car1">
+            <h1 style={{backgroundColor : "green", color : "yellow"}}>Bruttish mg</h1>
+            <img src="https://sm.ign.com/t/ign_nordic/review/b/black-myth/black-myth-wukong-review_xcgh.1024.jpg" alt="wokong img" />
+        </div>
+    )
+}
+export default Car
