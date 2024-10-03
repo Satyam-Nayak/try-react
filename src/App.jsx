c43522f8fc8a47a6c2ee10c2f5139d819bbccb90
Satyam-Nayak <satyamn120@gmail.com>
2024-10-04 00:39:09 +0530

    04-10-2024

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
 // import './App.css';
-// import Home from './Component1';
-// import Login from './Login';
-// import Ccompo2 from './Compo2';
-// import Car from './Car';
-// import Incre from './Incre';
-
-// import Hp from "./Hp";
-// import CashCounter from './Cash'
+// import other components if needed
 import Items1 from './Items1';
-// import Bike from './Bike';
+
 function App() {
   return (
     <div>
-      <Items1/>
-      {/* <Hp/> */}
-      {/* <CashCounter/> */}
-      {/* <Incre></Incre>
-      <Bike></Bike>
-      <Car></Car>
-    <Home></Home>
-    <Ccompo2></Ccompo2>  */}
-    {/* using class functions */}
-    {/* <Login></Login> */}
+      <Items1 />
+      {/* Uncomment to use other components */}
+      {/* <Hp /> */}
+      {/* <CashCounter /> */}
+      {/* <Incre /> */}
+      {/* <Bike /> */}
+      {/* <Car /> */}
+      {/* <Home /> */}
+      {/* <Ccompo2 /> */}
+      {/* <Login /> */}
     </div>
-  )
+  );
 }
 
 export default App;
--- src/Items1.jsx
+++ src/Items1.jsx
@@ -1,36 +1,26 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 function Items1() {
-  let [item, setItem] = useState([]);
+    let [item, setItem] = useState([]);
 
-  useEffect(() => {
     async function fetchData() {
-      try {
-        let data = await fetch('http://localhost:2626/Itmes'); // Correct the endpoint here if necessary
+        let data = await fetch('http://localhost:2626/Itmes');
         let res = await data.json();
         setItem(res);
-      } catch (error) {
-        console.log("Error fetching data:", error);
-      }
     }
-    fetchData();
-  }, []);
 
-  return (
-    <div className="items">
-      {item.length > 0 ? (
-        item.map((itm) => (
-          <div key={itm.id} className="item-card">
-            <h3>{itm.name}</h3>
-            <img src={itm.image} alt={itm.name} style={{ width: "200px", height: "auto" }} />
-            <p>Price: {itm.price}</p>
-          </div>
-        ))
-      ) : (
-        <p>No items found</p>
-      )}
-    </div>
-  );
+    useEffect(() => {
+        fetchData();
+    }, []); // Empty dependency array ensures fetchData is called only once on mount
+
+    console.log(item);
+
+    return (
+        <div className="items">
+            <h1>Items List</h1>
+            {/* Render items here */}
+        </div>
+    );
 }
 
 export default Items1;
